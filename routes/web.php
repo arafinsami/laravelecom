a2340e717a259826f0d85dbd9588cd75fca0c5e3
sami <sami.cse.1112@gmail.com>
2021-02-09 14:53:52 +0600

    frontend template setting up

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,11 @@ use Illuminate\Support\Facades\Auth;
 */
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/','IndexController@index');
 
 Auth::routes();
 
