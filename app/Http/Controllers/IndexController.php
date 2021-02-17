ee65efafe5dee560369313dc2c358110e9384829
sami <sami.cse.1112@gmail.com>
2021-02-17 14:18:29 +0600

    top rated, related and featured product showing

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -10,7 +10,8 @@ class IndexController extends Controller {
 
     public function index(){
         $products = Product::where('status',1)->latest()->get();
+        $latestProducts = Product::where('status',1)->latest()->limit(4)->get();
         $categories = Category::where('status',1)->latest()->get();
-        return view('frontend_pages.index',compact('products','categories'));
+        return view('frontend_pages.index',compact('products','categories','latestProducts'));
     }
 }
