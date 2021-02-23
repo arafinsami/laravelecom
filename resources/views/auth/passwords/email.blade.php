3cf26bbcf6618c5abad90218c06ef0a1304c3e9a
sami <sami.cse.1112@gmail.com>
2021-02-23 16:41:21 +0600

    login,logout and register done

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,6 +1,7 @@
-@extends('layouts.app')
+@extends('layouts.frontend_layouts')
 
 @section('content')
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
--- resources/views/layouts/frontend_layouts.blade.php
+++ resources/views/layouts/frontend_layouts.blade.php
@@ -119,7 +119,20 @@
                                 </ul>
                             </div>
                             <div class="header__top__right__auth">
-                                <a href="#"><i class="fa fa-user"></i> Login</a>
+                                @guest
+                                    <a href="{{ url('/login') }}"><i class="fa fa-sign-in"></i> Login</a>
+                                    <a href="{{ url('/register') }}"><i class="fa fa-sign-in"></i>Register</a>
+                                @else
+                                    <a href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                         document.getElementById('logout-form').submit();">
+                                        <i class="fa fa-sign-out"></i>
+                                        {{ __('Logout') }}
+                                    </a>
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                        style="display: none;">
+                                        @csrf
+                                    </form>
+                                @endguest
                             </div>
                         </div>
                     </div>
@@ -190,68 +203,68 @@
     <!-- Header Section End -->
 
 
-    @if(Request::path() == '/')
-    <!-- Hero Section Begin -->
-    <section class="hero">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-3">
-                    <div class="hero__categories">
-                        <div class="hero__categories__all">
-                            <i class="fa fa-bars"></i>
-                            <span>All categories</span>
-                            @php
-                                $categories = App\Category::where('status', 1)
-                                    ->latest()
-                                    ->get();
-                            @endphp
+    @if (Request::path() == '/')
+        <!-- Hero Section Begin -->
+        <section class="hero">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-3">
+                        <div class="hero__categories">
+                            <div class="hero__categories__all">
+                                <i class="fa fa-bars"></i>
+                                <span>All categories</span>
+                                @php
+                                    $categories = App\Category::where('status', 1)
+                                        ->latest()
+                                        ->get();
+                                @endphp
+                            </div>
+                            <ul>
+                                @foreach ($categories as $category)
+                                    <li><a href="#">{{ $category->categoryName }}</a></li>
+                                @endforeach
+                            </ul>
                         </div>
-                        <ul>
-                            @foreach ($categories as $category)
-                                <li><a href="#">{{ $category->categoryName }}</a></li>
-                            @endforeach
-                        </ul>
                     </div>
-                </div>
-                <div class="col-lg-9">
-                    <div class="hero__search">
-                        <div class="hero__search__form">
-                            <form action="#">
-                                <div class="hero__search__categories">
-                                    All Categories
-                                    <span class="arrow_carrot-down"></span>
+                    <div class="col-lg-9">
+                        <div class="hero__search">
+                            <div class="hero__search__form">
+                                <form action="#">
+                                    <div class="hero__search__categories">
+                                        All Categories
+                                        <span class="arrow_carrot-down"></span>
+                                    </div>
+                                    <input type="text" placeholder="What do yo u need?">
+                                    <button type="submit" class="site-btn">SEARCH</button>
+                                </form>
+                            </div>
+                            <div class="hero__search__phone">
+                                <div class="hero__search__phone__icon">
+                                    <i class="fa fa-phone"></i>
                                 </div>
-                                <input type="text" placeholder="What do yo u need?">
-                                <button type="submit" class="site-btn">SEARCH</button>
-                            </form>
-                        </div>
-                        <div class="hero__search__phone">
-                            <div class="hero__search__phone__icon">
-                                <i class="fa fa-phone"></i>
-                            </div>
-                            <div class="hero__search__phone__text">
-                                <h5>+65 11.188.888</h5>
-                                <span>support 24/7 time</span>
+                                <div class="hero__search__phone__text">
+                                    <h5>+65 11.188.888</h5>
+                                    <span>support 24/7 time</span>
+                                </div>
                             </div>
                         </div>
-                    </div>
 
-                   @if(Request::path() == '/')
-                       <div class="hero__item set-bg" data-setbg="{{ asset('frontend') }}/img/hero/banner.jpg">
-                        <div class="hero__text">
-                            <span>FRUIT FRESH</span>
-                            <h2>Vegetable <br />100% Organic</h2>
-                            <p>Free Pickup and Delivery Available</p>
-                            <a href="#" class="primary-btn">SHOP NOW</a>
-                        </div>
+                        @if (Request::path() == '/')
+                            <div class="hero__item set-bg" data-setbg="{{ asset('frontend') }}/img/hero/banner.jpg">
+                                <div class="hero__text">
+                                    <span>FRUIT FRESH</span>
+                                    <h2>Vegetable <br />100% Organic</h2>
+                                    <p>Free Pickup and Delivery Available</p>
+                                    <a href="#" class="primary-btn">SHOP NOW</a>
+                                </div>
+                            </div>
+                        @endif
+
                     </div>
-                   @endif
-                    
                 </div>
             </div>
-        </div>
-    </section>
-    <!-- Hero Section End -->
+        </section>
+        <!-- Hero Section End -->
     @endif
 
     @yield('content')
@@ -346,6 +359,7 @@
                 $(this).alert('close');
             });
         });
+
     </script>
 
 </body>
